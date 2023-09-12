7cc41df12f3e7b5c10eaf133d6e898f73453b3e1
rachytamago <144399492+rachytamago@users.noreply.github.com>
2023-09-12 17:41:53 +0900

    add onStart and onResume to listener

--- app/src/androidTest/java/com/example/shoppingapp/utils/ActivityChangeListener.kt
+++ app/src/androidTest/java/com/example/shoppingapp/utils/ActivityChangeListener.kt
@@ -8,13 +8,30 @@ import androidx.activity.ComponentActivity
  * Not confident with this implementation, just wanted to demonstrate the idea
  */
 class ActivityChangeListener : ComponentActivity() {
+    companion object {
+        const val TAG = "ActivityChangeListener"
+    }
     // Makes it possible to get the activity from the rule
     var currentActivity: Activity? = null
 
     // Override the onCreate method to log activity changes
     override fun onCreate(savedInstanceState: android.os.Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("ActivityChangeListener", "Activity Created: $this")
+        Log.d(TAG, "Activity Created: $this")
+        currentActivity = this
+    }
+
+    // Override the onStart method to log when the test starts the activity
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "Activity Started: $this")
+        currentActivity = this
+    }
+
+     // Override the onStart method to log when the test returns to the activity
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "Activity Resumed: $this")
         currentActivity = this
     }
 
